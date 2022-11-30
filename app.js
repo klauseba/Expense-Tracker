ffadc1ba562bdb49c0d8a5e7167f05332791125c
klauseba <66975395+klauseba@users.noreply.github.com>
2022-11-30 10:30:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ window.addEventListener("load", () => {
   const amount = document.querySelector("#amount-input-id");
   const table = document.querySelector(".content-table");
   const tableBody = document.querySelector(".tbody-class");
+  const tdataClass = document.querySelector(".tdata-class");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -13,39 +14,59 @@ window.addEventListener("load", () => {
     const dateTable = date.value;
     const amountTable = amount.value;
 
-    // the new table that will be inserted
-    //creating the table row and the first table data
+    // the new table that will be inserted when clicking the Add Expense button
+    //creating the table row ("tr") that will hold every table data ("td")
     const TR1 = document.createElement("tr");
-    const TD1 = document.createElement("td");
-    TR1.appendChild(TD1);
+    TR1.classList.add("tableRow1");
 
+    //name column's td element
+    const TD1 = document.createElement("td");
+    TD1.classList.add("tableData1");
+    TR1.appendChild(TD1);
     //name column
     const firstName = document.createElement("output");
-    firstName.classList.add("text");
+    firstName.classList.add("nameData1");
     firstName.type = "text";
     firstName.value = nameTable;
     TD1.appendChild(firstName);
 
-    //date column
+    //date column's td element
     const TD2 = document.createElement("td");
+    TD2.classList.add("td-div");
     TR1.appendChild(TD2);
+    //date column
     const firstDate = document.createElement("output");
     firstDate.type = "date";
     firstDate.value = dateTable;
+    TD2.classList.add("tableData2");
     TD2.appendChild(firstDate);
 
-    // amount column
+    //amount column's td element
     const TD3 = document.createElement("td");
+    TD3.classList.add("td-div2");
     TR1.appendChild(TD3);
+    //amount column
     const firstAmount = document.createElement("output");
-    firstAmount.classList.add("text");
+    firstAmount.classList.add("amountClass");
     firstAmount.type = "number";
     firstAmount.value = amountTable;
     TD3.appendChild(firstAmount);
 
-    //appending them together
+    //del button's td element
+    const TD4 = document.createElement("td");
+    TD4.classList.add("deleteButtonTd");
+    TR1.appendChild(TD4);
+    //del button
+    const task_delete_el = document.createElement("button");
+    task_delete_el.classList.add("deleteButtonClass");
+    task_delete_el.innerText = "X";
+    TD4.appendChild(task_delete_el);
+
     tableBody.appendChild(TR1);
 
-    input.value = "";
+    // when clicking on delete it will delete the whole row:
+    task_delete_el.addEventListener("click", (e) => {
+      tableBody.removeChild(TR1);
+    });
   });
 });
